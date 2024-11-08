9965235ca0628b6484f8219118b6ae68e3e43a78
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-11-08 22:57:29 +0100

    restyling UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,30 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ItemCard from "./ItemCard";
-import image1 from "/images/foodiesfeed.com_bowl-of-ice-cream-with-chocolate.jpg";
-import image2 from "/images/foodiesfeed.com_bowl-of-ice-cream-with-chocolate.jpg";
-import image3 from "/images/foodiesfeed.com_bowl-of-ice-cream-with-chocolate.jpg";
+
 const menu = [
   {
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
     price: 15.99,
-    img: image1,
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    img: "/img/foodiesfeed.com_bowl-of-ice-cream-with-chocolate.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`,
   },
   {
     id: 2,
     title: "diner double",
     category: "lunch",
     price: 13.99,
-    img: image2,
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    img: "/img/burger.jfif",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats`,
   },
   {
     id: 3,
     title: "godzilla milkshake",
     category: "shakes",
     price: 6.99,
-    img: image3,
+    img: "/img/dorucak.jfif",
     desc: `kombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
@@ -36,23 +34,23 @@ const menu = [
     title: "country delight",
     category: "breakfast",
     price: 20.99,
-    img: "/images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    img: "/img/palacinka.jfif",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut`,
   },
   {
     id: 5,
     title: "egg attack",
     category: "lunch",
     price: 22.99,
-    img: "/images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    img: "/img/salatica.jfif",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up`,
   },
   {
     id: 6,
     title: "oreo dream",
     category: "shakes",
     price: 18.99,
-    img: "/images/item-6.jpeg",
+    img: "/img/spagete.jfif",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
@@ -60,23 +58,23 @@ const menu = [
     title: "bacon overflow",
     category: "breakfast",
     price: 8.99,
-    img: "/images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    img: "/img/tzimmes.webp",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird`,
   },
   {
     id: 8,
     title: "american classic",
     category: "lunch",
     price: 12.99,
-    img: "/images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    img: "public/images/foodiesfeed.com_hot-chocolate.jpg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut`,
   },
   {
     id: 9,
     title: "quarantine buddy",
     category: "shakes",
     price: 16.99,
-    img: "/images/item-9.jpeg",
+    img: "/img/foodiesfeed.com_bowl-of-ice-cream-with-chocolate.jpg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
